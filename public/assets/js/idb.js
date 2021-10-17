f1a96ccfa8ff25b06c09942384e164d1e1339691
Harborth, Allen <allenharborth9835@gmail.com>
2021-10-17 16:09:04 -0500

    finish offline functionality

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -11,10 +11,55 @@ request.onsuccess = function(event){
   db = event.target.result;
 
   if(navigator.onLine){
-
+    uploadPizza();
   }
 };
 
 request.onerror = function(event){
   console.log(event.target.errorCode);
 };
+
+function saveRecord(record){
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  pizzaObjectStore.add(record);
+}
+
+function uploadPizza(){
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  const getAll = pizzaObjectStore.getAll();
+
+  getAll.onsuccess = function(){
+    if(getAll.result.length > 0){
+      fetch('/api/pizzas',{
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        header:{
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if(serverResponse.message){
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['new_pizza'], 'readwrite');
+          const pizzaObjectStore = transaction.objectStore('new_pizza');
+          pizzaObjectStore.clear();
+
+          alert('All saved pizza has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      }
+  }
+}
+
+window.addEventListener('online', uploadPizza)
